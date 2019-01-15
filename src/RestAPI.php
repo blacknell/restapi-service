8e65e54b3878b334b1a99c771c1295aed0d7a96e
Paul Blacknell <blik2108.shop@fastmail.co.uk>
2019-01-15 11:27:38 +0000

    Added result codes

--- src/RestAPI.php
+++ src/RestAPI.php
@@ -201,6 +201,8 @@ abstract class RestAPI
 	{
 		$status = array(
 			200 => 'OK',
+			201 => 'Created',
+			202 => 'Accepted',
 			400 => 'Bad Request',
 			401 => 'Unauthorized',
 			403 => 'Forbidden',
