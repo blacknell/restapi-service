99f537a22507fb9a08e8226445e2da434701817a
Paul Blacknell <blik2108.shop@fastmail.co.uk>
2021-02-02 10:59:25 +0000

    various
    chore: update packages
    chore: prettify & address php syntax
    add: further HTTP response codes

--- src/RestAPI.php
+++ src/RestAPI.php
@@ -63,8 +63,7 @@ abstract class RestAPI
     {
         if (isset($logger) && $logger) {
             $this->logger = $logger;
-        }
-        else {
+        } else {
             $this->logger = new Logger('');
             $logHandler = new NullHandler();
             $this->logger->pushHandler($logHandler);
@@ -82,11 +81,9 @@ abstract class RestAPI
         if ($this->method == 'POST' && array_key_exists('HTTP_X_HTTP_METHOD', $_SERVER)) {
             if ($_SERVER['HTTP_X_HTTP_METHOD'] == 'DELETE') {
                 $this->method = 'DELETE';
-            }
-            elseif ($_SERVER['HTTP_X_HTTP_METHOD'] == 'PUT') {
+            } elseif ($_SERVER['HTTP_X_HTTP_METHOD'] == 'PUT') {
                 $this->method = 'PUT';
-            }
-            else {
+            } else {
                 $this->logger->notice("Unexpected Header", $this->toObject());
                 throw new RuntimeException("Unexpected Header", 400);
             }
@@ -172,10 +169,11 @@ abstract class RestAPI
     private function response($data, $status = 200)
     {
         @header("HTTP/1.1 " . $status . " " . $this->requestStatus($status));
-        if (is_object($data))
+        if (is_object($data)) {
             return $data;
-        else
+        } else {
             return json_encode($data);
+        }
     }
 
     /**
@@ -185,13 +183,12 @@ abstract class RestAPI
      */
     private function cleanInputs($data)
     {
-        $cleanInput = Array();
+        $cleanInput = array();
         if (is_array($data)) {
             foreach ($data as $k => $v) {
                 $cleanInput[$k] = $this->cleanInputs($v);
             }
-        }
-        else {
+        } else {
             $cleanInput = trim(strip_tags($data));
         }
 
@@ -201,25 +198,69 @@ abstract class RestAPI
     /**
      * @param $code
      *
-     * @return mixed
+     * @return string
      */
-    private function requestStatus($code)
+    private function requestStatus($code): string
     {
         $status = array(
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            102 => 'Processing',
             200 => 'OK',
             201 => 'Created',
             202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
             204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            207 => 'Multi-status',
+            208 => 'Already Reported',
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            306 => 'Switch Proxy',
+            307 => 'Temporary Redirect',
             400 => 'Bad Request',
             401 => 'Unauthorized',
+            402 => 'Payment Required',
             403 => 'Forbidden',
             404 => 'Not Found',
             405 => 'Method Not Allowed',
             406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Time-out',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
             412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Large',
             415 => 'Unsupported Media Type',
+            416 => 'Requested range not satisfiable',
+            417 => 'Expectation Failed',
+            418 => 'I\'m a teapot',
+            422 => 'Unprocessable Entity',
+            423 => 'Locked',
+            424 => 'Failed Dependency',
+            425 => 'Unordered Collection',
+            426 => 'Upgrade Required',
+            428 => 'Precondition Required',
+            429 => 'Too Many Requests',
+            431 => 'Request Header Fields Too Large',
+            451 => 'Unavailable For Legal Reasons',
             500 => 'Internal Server Error',
             501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Time-out',
+            505 => 'HTTP Version not supported',
+            506 => 'Variant Also Negotiates',
+            507 => 'Insufficient Storage',
+            508 => 'Loop Detected',
+            511 => 'Network Authentication Required',
         );
 
         return ($status[$code]) ? $status[$code] : $status[500];
@@ -227,4 +268,3 @@ abstract class RestAPI
 }
 
 //warning - do not let any output after this closing brace
-?>
